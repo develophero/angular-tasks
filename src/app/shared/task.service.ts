589d57fc3279e88870e4635836e7585e4a631585
zhicks <zackrhicks@gmail.com>
2017-02-23 14:33:23 -0500

    task service with cache. unimplemented getByid. Observables

--- src/app/shared/task.service.ts
+++ src/app/shared/task.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Http } from "@angular/http";
+import {Observable} from "rxjs";
+import {Http} from "@angular/http";
 
 export interface Task {
   id: number,
@@ -10,12 +11,31 @@ export interface Task {
 @Injectable()
 export class TaskService {
 
+  private tasksCache: Task[];
+
   constructor(
     private http: Http
   ) { }
 
   getTasks() {
-    return this.http.get('/tasks.json').map(res => res.json());
+    return new Observable<Task[]>((observer) => {
+      if (this.tasksCache) {
+        observer.next(this.tasksCache);
+        observer.complete();
+      } else {
+        this.http.get('/tasks.json').map(res => res.json()).subscribe((tasks) => {
+          this.tasksCache = tasks;
+          observer.next(this.tasksCache);
+          observer.complete();
+        });
+      }
+    });
+  }
+
+  getById(id: number) {
+    // A "cheat" method for now - we get all the tasks again and just return the one
+    // with that id. Note that without modifications this won't work with new tasks you've created!
+    // TODO: Implement
   }
 
 }
