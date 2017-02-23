47542e889476588ef5cb56f59d576819a1286753
zhicks <zackrhicks@gmail.com>
2017-02-23 14:37:24 -0500

    edit task complete

--- src/app/task-edit/task-edit.component.ts
+++ src/app/task-edit/task-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Subscription} from "rxjs";
+import {ActivatedRoute, Router} from "@angular/router";
+import {TaskService, Task} from "../shared/task.service";
 
 @Component({
   selector: 'app-task-edit',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskEditComponent implements OnInit {
 
-  constructor() { }
+  task: Task;
+  routeSubscription: Subscription;
+  editingContent: string;
+
+  constructor(
+    private route: ActivatedRoute,
+    private taskService: TaskService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.routeSubscription = this.route.params.subscribe(params => {
+      let id = +params['id'];
+      this.taskService.getById(id).subscribe((task) => {
+        this.task = task;
+        this.editingContent = task.content;
+      });
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.routeSubscription) {
+      this.routeSubscription.unsubscribe();
+    }
+  }
+
+  saveTaskClicked() {
+    if (this.editingContent) {
+      this.task.content = this.editingContent;
+      this.router.navigate(['/tasks']);
+    }
+  }
+
+  cancelClicked() {
+    this.router.navigate(['/tasks']);
   }
 
 }
